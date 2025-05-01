d9349546f67ee8b910f7aa2ca0fad832dfa8aa9b
sakhawatashnaiy <sakhawatashnaiy09@gmail.com>
2025-05-01 09:20:10 +0500

    class-12

---
+++ class-12/app.js
@@ -0,0 +1,41 @@
+ 
+//  function colors(){
+//       let data = 20;
+//       console.log(data);
+//  }
+//    function  days(){
+//       colors();
+//    }
+
+//    function fruits(){
+//       days();
+//    }
+//    function Toptfunction(){
+//       fruits();
+//    }
+//     Toptfunction()
+
+
+    //   head recusion 
+
+    //    function test(x){
+    //       console.log(x);
+    //       if(x>0){
+    //           test(x-1);
+    //       };
+    //    }
+
+    //     let data = 5;
+    //     test(data);
+      
+
+        //    Tail Recursion
+
+        //    function test(x){
+        //       if(x>0){
+        //          test(x-1);
+        //       }
+        //        console.log(x);
+        //    }
+        //     let data1 = 5;
+        //     test(data1);
